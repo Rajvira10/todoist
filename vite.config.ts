b345153c4e7f2506a9a88a5c7da17542bb549716
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2024-10-13 21:47:54 +0600

    minor change

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
-});
+export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  process.env = { ...process.env, ...env };
+  return defineConfig({
+    plugins: [
+      remix({
+        future: {
+          v3_fetcherPersist: true,
+          v3_relativeSplatPath: true,
+          v3_throwAbortReason: true,
+        },
+      }),
+      tsconfigPaths(),
+    ],
+  });
+};
